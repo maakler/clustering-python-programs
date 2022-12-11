2c1b26423c49c40a286d7024d33f0787dad21c65
maakler <73598932+maakler@users.noreply.github.com>
2022-12-11 20:39:11 +0200

    top students

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import seaborn as sns
 import numpy as np
 from sklearn.metrics.pairwise import linear_kernel
 
-data_contents = read_python_files("data/test2")
+
+data_contents = read_python_files("data/test1")
+
+student_codes = []
 
 tokens = [ast_to_tokens(file) for file in data_contents]
+
 
 vectorizer = make_vectorizer(tokens)
 
 S = vectorizer.transform(tokens)
+
 print(vectorizer.vocabulary_) # Vocabulary built is inside vectorizer.vocabulary_
 
 tfm = linear_kernel(S, S)
@@ -20,3 +25,21 @@ fig = thm.get_figure()
 fig.savefig("results/tfidf_heatmap2.png", dpi=150)
 
 np.savetxt("results/tfidf", tfm, fmt="%.4f", delimiter=',')
+
+max_values = []
+for i, row in enumerate(tfm):
+    row[i] = 0
+    max_value = max(row)
+    max_values.append(("student " + str(student_codes[i]), max_value, row))
+
+most_similar = sorted(max_values, key=lambda x: x[1])
+most_similar.reverse()
+least_similar = sorted(max_values, key=lambda x: x[1])
+
+print("Most similar:")
+for i in most_similar[:20]:
+    print(i[0], i[1])
+
+print("Least similar:")
+for i in least_similar[:20]:
+    print(i[0], i[1])
