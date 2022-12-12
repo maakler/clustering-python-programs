9a74d1178c227d5e506c126bd5c17275fc1fac96
maakler <73598932+maakler@users.noreply.github.com>
2022-12-13 00:30:18 +0200

    added functionality to see how many times 2 student submitted homeworks for same tasks

--- analysis.py
+++ analysis.py
@@ -28,27 +28,28 @@ def read_python_files(data_path):
 
 
 def ast_to_tokens(pyfile_content: str, visitor=unplag.Visitor()) -> list:
-    """Returns tokens from the `visitor` after it visits `pyfile_content`. `visitor` defaults to `cyclomatic.Visitor()`"""
+    """Returns tokens from the `visitor` after it visits `pyfile_content`. `visitor` defaults to `cyclomatic.Visitor(
+    )` """
     try:
         root = ast.parse(pyfile_content)
         visitor.visit(root)
         names = [node.id for node in ast.walk(root) if isinstance(node, ast.Name)]
-        return names
+        return visitor.tokens + names
     except Exception:
-        return ["ERROR"]
+        return []
 
 
 def make_vectorizer(tokens: list):
-    """Trains and returns the vectorizer. `tokens` should be a list of collections of tokens, e.g. `[['token1', 'token2'], ['token1']]`"""
+    """Trains and returns the vectorizer. `tokens` should be a list of collections of tokens, e.g. `[['token1',
+    'token2'], ['token1']]` """
     I = lambda x: x
     vectorizer = TfidfVectorizer(
         analyzer="word",
         tokenizer=I,
         preprocessor=I,
         # Consider unigrams, bigrams and trigrams, including trigrams works better for python files
-        ngram_range=(1, 3),
+        ngram_range=(1, 2),
         sublinear_tf=True,  # (1+log(tf)) instead of just tf
-        max_features=5000,
         encoding="utf-8",
         decode_error="ignore",
         stop_words=None,
@@ -68,12 +69,13 @@ def get_clusters(vectors, clustering=KMeans, **kwargs):
     return model.fit_predict(vectors)
 
 
-def get_similarities(vectors, student_codes):
+def get_similarities(vectors, submit_matches, student_codes):
     max_values = []
     for i, row in enumerate(vectors):
         row[i] = 0
-        max_index = np.argmax(row)
-        max_values.append((row[max_index], student_codes[i], student_codes[max_index]))
+        max_indexes = ind = np.argpartition(row, -2)[-2:]
+        for max_index in max_indexes:
+            max_values.append((row[max_index], student_codes[i], student_codes[max_index], submit_matches[i][max_index]))
 
     most_similar = sorted(max_values, key=lambda x: x[0])
     most_similar.reverse()
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sklearn.metrics.pairwise import linear_kernel
 
 scm = get_similarity_count_matrix(377)
 
-for K in range(1, 16):
+for K in range(14, 15):
     for k in range(1, 5):
         num = "0" * (2 - len(str(K))) + str(K)
         dir_name = "data/K" + num + "kodu" + str(k)
@@ -19,14 +19,14 @@ for K in range(1, 16):
 
         data_contents, student_codes = read_python_files(dir_name)
 
-        tokens = [ast_to_tokens(file) for file in data_contents]
+        tokens = [result for file in data_contents for result in [ast_to_tokens(file)] if result]
 
         vectorizer = make_vectorizer(tokens)
 
         # print(vectorizer.vocabulary_)  # Vocabulary built is inside vectorizer.vocabulary_
         vectors = vectorizer.transform(tokens)
 
-        clusters = get_clusters(vectors, n_clusters=2)
+        clusters = get_clusters(vectors, n_clusters=10)
 
         run_vis(vectors, clusters)
 
@@ -36,12 +36,12 @@ for K in range(1, 16):
 
         tfm = linear_kernel(vectors, vectors)
 
-        # TF-IDF Heatmap
-        thm = sns.heatmap(tfm)
-        fig = thm.get_figure()
-        fig.savefig("results/varnames_K" + num + "kodu" + str(k) + ".png", dpi=150)
-
-        np.savetxt("results/varnames_K" + num + "kodu" + str(k), tfm, fmt="%.4f", delimiter=',')
+        # # TF-IDF Heatmap
+        # thm = sns.heatmap(tfm)
+        # fig = thm.get_figure()
+        # fig.savefig("results/varnames_K" + num + "kodu" + str(k) + ".png", dpi=150)
+        #
+        # np.savetxt("results/varnames_K" + num + "kodu" + str(k), tfm, fmt="%.4f", delimiter=',')
 
         for i, row in enumerate(tfm):
             s1 = student_codes[i][:-3]
@@ -50,27 +50,52 @@ for K in range(1, 16):
                 scm[s1][s2][0] += 1
                 scm[s1][s2][1] += student2
 
+# Similarity matrix
 S = []
+# number of times these 2 students submitted for same hw
+S_count = []
 student_codes = []
 for key, value in scm.items():
     temp = []
+    counts = []
     student_codes.append(key)
     for tup in value:
         if tup[0] == 0:
+            counts.append(0)
             temp.append(0)
         else:
+            counts.append(tup[0])
             temp.append(tup[1] / tup[0])
+
+    S_count.append(counts)
     S.append(temp)
+
+# TF-IDF Heatmap
+thm = sns.heatmap(S)
+fig = thm.get_figure()
+fig.savefig("results/results_varnames", dpi=150)
+
+np.savetxt("results/results_varnames", S, fmt="%.4f", delimiter=',')
 
 student_codes = ["S" + "0" * (3 - len(str(i))) + str(i) for i in range(1, 377 + 1)]
 
-most_similar, least_similar = get_similarities(S, student_codes)
+most_similar, least_similar = get_similarities(S, S_count, student_codes)
 
 print("EPIC FINALLLY")
 print("Most similar:")
-for i in most_similar[:20]:
-    print(i[0], "- ", i[1], i[2])
+prev = []
+k = 0
+for i in most_similar:
+    if i[0] not in prev and i[3] > 1:
+        k += 1
+        print(i[0], "- ", i[1], i[2], "   num of hw matches", i[3])
+    prev.append(i[0])
+    if k>200:
+        break
 
 print("Least similar:")
+prev = 69
 for i in least_similar[:20]:
-    print(i[0], "- ", i[1], i[2])
+    if i != prev:
+        print(i[0], "- ", i[1], i[2], "   num of hw matches", i[3])
+    prev = i[0]
