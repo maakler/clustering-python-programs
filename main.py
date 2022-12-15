48ffbe3dd7cc0d461b12669dcb0df928a3a59180
maakler <73598932+maakler@users.noreply.github.com>
2022-12-15 12:41:37 +0200

    Added comments and cleaned up the code a bit

--- main.py
+++ main.py
@@ -5,54 +5,68 @@ import seaborn as sns
 import numpy as np
 from sklearn.metrics.pairwise import linear_kernel
 
+# matrix for every student where all the similairies are added together
 scm = get_similarity_count_matrix(377)
+
+dir_names = []
 
 for K in range(14, 15):
     for k in range(1, 5):
         num = "0" * (2 - len(str(K))) + str(K)
         dir_name = "data/K" + num + "kodu" + str(k)
+        dir_names.append(dir_name)
 
-        if not os.path.isdir(dir_name):
-            continue
+for dir_name in dir_names:
+    # check if directory is valid
+    if not os.path.isdir(dir_name):
+        continue
+    print(dir_name)
 
-        print(dir_name)
+    # read in all python files and student codes for all the students that submitted a solution for this homework
+    data_contents, student_codes = read_python_files(dir_name)
 
-        data_contents, student_codes = read_python_files(dir_name)
+    # tokenize every python file  example: tokens = [["call", "if", ... , "var_name"], [...], [...]]
+    tokens = [result for file in data_contents for result in [ast_to_tokens(file)] if result]
 
-        tokens = [result for file in data_contents for result in [ast_to_tokens(file)] if result]
+    # TF-IDF
+    vectorizer = make_vectorizer(tokens)
 
-        vectorizer = make_vectorizer(tokens)
+    # print(vectorizer.vocabulary_)  # Vocabulary built is inside vectorizer.vocabulary_
+    vectors = vectorizer.transform(tokens)
 
-        # print(vectorizer.vocabulary_)  # Vocabulary built is inside vectorizer.vocabulary_
-        vectors = vectorizer.transform(tokens)
+    # make clusters with KMeans
+    clusters = get_clusters(vectors, n_clusters=10)
 
-        clusters = get_clusters(vectors, n_clusters=10)
+    # plot clusters
+    run_vis(vectors, clusters)
+    plt.show()
+    # plt.savefig("results/clusters_K" + num + "kodu" + str(k) + ".png")
 
-        run_vis(vectors, clusters)
+    tfm = linear_kernel(vectors, vectors)
 
-        # show the plot
-        plt.show()
-        # plt.savefig("results/clusters_K" + num + "kodu" + str(k) + ".png")
+    # # TF-IDF Heatmap
+    # thm = sns.heatmap(tfm)
+    # fig = thm.get_figure()
+    # fig.savefig("results/varnames_K" + num + "kodu" + str(k) + ".png", dpi=150)
+    #
+    # np.savetxt("results/varnames_K" + num + "kodu" + str(k), tfm, fmt="%.4f", delimiter=',')
 
-        tfm = linear_kernel(vectors, vectors)
+    # add up the similarities between every student that submitted a solution and count how many times the 2 students
+    # submitted a solution for the same homework
+    for i, row in enumerate(tfm):
+        # student code 1
+        s1 = student_codes[i][:-3]
+        for j, student2 in enumerate(row):
+            # student code 2
+            s2 = int(student_codes[j][1:-3]) - 1
+            # +1 so we know that both of these students submitted the homework
+            scm[s1][s2][0] += 1
+            # add up the similarities for these 2 students
+            scm[s1][s2][1] += student2
 
-        # # TF-IDF Heatmap
-        # thm = sns.heatmap(tfm)
-        # fig = thm.get_figure()
-        # fig.savefig("results/varnames_K" + num + "kodu" + str(k) + ".png", dpi=150)
-        #
-        # np.savetxt("results/varnames_K" + num + "kodu" + str(k), tfm, fmt="%.4f", delimiter=',')
 
-        for i, row in enumerate(tfm):
-            s1 = student_codes[i][:-3]
-            for j, student2 in enumerate(row):
-                s2 = int(student_codes[j][1:-3]) - 1
-                scm[s1][s2][0] += 1
-                scm[s1][s2][1] += student2
-
-# Similarity matrix
+# Similarity matrix across all submitted homeworks
 S = []
-# number of times these 2 students submitted for same hw
 S_count = []
 student_codes = []
 for key, value in scm.items():
@@ -60,9 +74,11 @@ for key, value in scm.items():
     counts = []
     student_codes.append(key)
     for tup in value:
+        # these 2 students or one of the students didn't submit anything and were not comparable
         if tup[0] == 0:
             counts.append(0)
             temp.append(0)
+        # divide the sum of the homework similarities with the number of times these students could be compared
         else:
             counts.append(tup[0])
             temp.append(tup[1] / tup[0])
@@ -74,14 +90,12 @@ for key, value in scm.items():
 thm = sns.heatmap(S)
 fig = thm.get_figure()
 fig.savefig("results/results_varnames", dpi=150)
-
 np.savetxt("results/results_varnames", S, fmt="%.4f", delimiter=',')
 
+# print highest and lowest similarities between all the students
 student_codes = ["S" + "0" * (3 - len(str(i))) + str(i) for i in range(1, 377 + 1)]
-
 most_similar, least_similar = get_similarities(S, S_count, student_codes)
 
-print("EPIC FINALLLY")
 print("Most similar:")
 prev = []
 k = 0
